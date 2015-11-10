979cbea4fda617dd896e89ed4788b32664c375ce
Willie Richardson <willie.richardson72@gmail.com>
2015-11-09 20:34:31 -0800

    new

---
+++ script.js
@@ -0,0 +1,22 @@
+var StarWarsPhotos = []
+
+function StarWarsPhoto (name, path) {
+  this.name=name;
+  this.path=path;
+  this.votes=0
+  images.push(this)
+}
+
+}
+var tracker= {
+  getRandomNum: function (){
+}
+  getRandomImg: function () {
+  for (var i=0; i<StarWarsPhotos.length; i++) {
+    StarWarsPhotos[i]
+  };
+}
+
+var vader = new Picture(name, path);
+var kenobi = new Picture(name, path);
+StarWarsPhotos();
