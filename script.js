bd14fc92f886d1603f162dee2e6947cd7056c8cb
Willie Richardson <willie.richardson72@gmail.com>
2015-11-12 19:54:34 -0800

    new

--- script.js
+++ script.js
@@ -19,9 +19,6 @@ function StarWarsPhoto (name, path) {
 
 }
 
-// function GetRandomPhotos () {
-//   return Math.floor(Math.random() * starWarsPhotos.length)
-// }
 
 function PhotoSelector () {
 
@@ -34,23 +31,30 @@ function PhotoSelector () {
   chooseTwo.src=choiceTwo.path;
 }
 
+// function checkLocal () {
+//   if (localStorage.UpdateChart && localStorage)
+// }
+
 function Voteone () {
   console.log('Jar Jar Binks is a Jedi');
   response.innerHTML='Jar Jar Binks is a Jedi!';
   choiceOne.value+=1;
   UpdateChart();
-  response.innerHTML=choiceOne.value;
   PhotoSelector();
-
+  localStorage.setItem('starWarsPhotos', JSON.stringify(starWarsPhotos));
+  PhotoSelector();
+  // localStorage.setItem('theChart', JSON.stringify(theChart));
 }
 
 function Votetwo () {
   console.log('Jar Jar Binks is a Sith Lord');
   response.innerHTML='Jar Jar Binks is a Sith Lord!';
   choiceTwo.value+=1;
-  UpdateChart()
-  response.innerHTML=choiceTwo.value;
+  UpdateChart();
   PhotoSelector();
+  localStorage.setItem('starWarsPhotos', JSON.stringify(starWarsPhotos));
+  PhotoSelector();
+  // local
 }
 
 function UpdateChart () {
@@ -75,11 +79,61 @@ var jabba=new StarWarsPhoto('Jabba','Img/jabba.png');
 var lando=new StarWarsPhoto('Lando Calrissian','Img/lando.jpg');
 var maul=new StarWarsPhoto('Darth Maul', 'Img/maul.jpg');
 
+// localStorage.setItem('starWarsPhotos', JSON.stringify(starWarsPhotos));
+function CheckLocal () {
+  if (localstorage.starWarsPhotos) {
+    starWarsPhotos=JSON.parse(localstorage.starWarsPhotos)
+  } else {
+  var vader=new StarWarsPhoto('Darth Vader', 'Img/Vader.jpg');
+  var kenobi=new StarWarsPhoto('Obi-Wan Kenobi', 'Img/ObiWan.jpg');
+  var jarjar=new StarWarsPhoto('Jar Jar Binks', 'Img/jarjar.jpg');
+  var boba=new StarWarsPhoto('Boba Fett', 'Img/boba.jpg');
+  var hansolo=new StarWarsPhoto('Han Solo', 'Img/han-solo.jpeg');
+  var chewy=new StarWarsPhoto('Chewbacca', 'Img/Chewy.jpg');
+  var leia=new StarWarsPhoto('Princess Leia','Img/leia.png');
+  var yoda=new StarWarsPhoto('Yoda', 'Img/Yoda.jpg');
+  var luke=new StarWarsPhoto('Luke Skywalkeer', 'Img/luke.jpg');
+  var jabba=new StarWarsPhoto('Jabba','Img/jabba.png');
+  var lando=new StarWarsPhoto('Lando Calrissian','Img/lando.jpg');
+  var maul=new StarWarsPhoto('Darth Maul', 'Img/maul.jpg'); }
+}
+
+
 PhotoSelector();
 console.log(choiceOne);
 
 chooseOne.addEventListener('click', Voteone);
 chooseTwo.addEventListener('click', Votetwo);
+
+
+// stateVoting() {
+//   two new random numbers
+//   display the PhotoSelector
+//   hide the chart?
+//   turn off or hide the '2 more photos' button
+// }
+
+// stateResults() {
+//   tally votes
+//   render the chart
+//   (local storage)
+//   turn off the event listeners on the pictures to prevent further Voting
+//   turn on or show the '2 more photos button'
+// }
+
+// picleft.addEventListener('click', stateResults);
+// picRight.addEventListener('click', stateResults);
+
+// Another way to manage stateResults
+
+// if (voting) {
+//   thing 1
+//   thing 2
+// } else {
+//   other thing 1
+//   other thing 2
+// }
+
 
 
 
